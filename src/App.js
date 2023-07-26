82c709d7508a1812ad9a8a2dff73f21e24b52389
Sarmat Totikov <Sarmat001002003@gmail.com>
2023-07-26 16:45:36 +0300

    Upd buttons + added ProductList

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import './App.css';
-import {useEffect} from "react";
-import {useTelegram} from "./hooks/useTelegram";
-import Header from "./components/Header/Header"
-import {Route, Routes} from 'react-router-dom'
+import { useEffect } from "react";
+import { useTelegram } from "./hooks/useTelegram";
+import Header from "./components/Header/Header";
+import { Route, Routes } from 'react-router-dom';
 import ProductList from './components/ProductList/ProductList';
 import Form from './components/Form/Form';
 
 const tg = window.Telegram.WebApp;
 
 function App() {
-    const {onToggleButton, tg} = useTelegram();
+    const { onToggleButton, tg } = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, [])
+    }, []);
 
-  return (
-    <div className="App">
-        <Header />
-        <Routes>
-            <Route index element={<ProductList />}/>
-            <Route path={'/form'} element={<Form />}/>
-        </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Routes>
+                <Route index element={<ProductList />} />
+                <Route path={'/form'} element={<Form />} />
+                <Route path="/product/:productId" component={ProductDetails} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './ProductList.css';
+import image1 from '../../images/image11.jpg';
 
 const ProductList = () => {
-
+    // Sample data for picture buttons
+    const products = [
+        { id: 1, name: 'Ozon', image: image1 },  // Use the imported image1
+        { id: 2, name: 'WB', image: image1 },    // Use the imported image1
+        { id: 3, name: 'Lenta', image: image1 }, // Use the imported image1
+        { id: 4, name: 'Andreyka', image: image1 } // Use the imported image1
+        // Add more products as needed
+    ];
     return (
-        <div>
-            ProductList
+        <div className="product-list">
+            {products.map((product) => (
+                <Link to={`/product/${product.id}`} key={product.id}>
+                    <div className="product-button">
+                        <img src={product.image} alt={product.name} />
+                        <span>{product.name}</span>
+                    </div>
+                </Link>
+            ))}
         </div>
-    )
-}
+    );
+};
 
 export default ProductList;
